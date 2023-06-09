8055c483a56277659f5cabcafd2ad9b5190e4520
coldblot <62607558+coldblot@users.noreply.github.com>
2023-06-09 19:53:46 +0100

    server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname+'/html'));
+app.get('/', (req, res) => res.send('hello'));
+const server = app.listen(port () => console.log('Server Start!!!'));
+
+const io = require('socket.io')(server);
+
+allPlayers = [];
+
