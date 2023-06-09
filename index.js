eaa349b5d9529f911a5a9dafe8ef7bd7c3a98556
Coldblot70 <124889839+Coldblot70@users.noreply.github.com>
2023-06-09 19:58:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname+'/html'));
 app.get('/', (req, res) => res.send('hello'));
 const server = app.listen(port () => console.log('Server Start!!!'));
 
