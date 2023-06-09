d9c53fa649fdfd802354fa9cf02a325ad18ac7a8
coldblot <62607558+coldblot@users.noreply.github.com>
2023-06-09 19:33:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const host = '192.168.193.240';
+const host = '127.0.0.1';
 const port = 3000;
 
 app.use(express.static(__dirname+'/html'));
