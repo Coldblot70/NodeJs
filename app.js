677491aecba20214f8403cb84a040d727c1fe9e0
coldblot <62607558+coldblot@users.noreply.github.com>
2023-06-09 19:48:56 +0100

    as

--- app.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-//fs.mkdirSync('html');
-//fs.writeFileSync('html/index.html', '');
-//fs.writeFileSync('html/style.css', '');
-//fs.writeFileSync('html/javascript.js', '');
-
-
-fs.writeFileSync('html/snake.js','');
