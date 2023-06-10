4f1adf849627840ea56e118f3fcf6dff844c9c2d
Coldblot70 <124889839+Coldblot70@users.noreply.github.com>
2023-06-10 18:21:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const app = express();
 const host = '0.0.0.0';
 const port = process.env.SERVER_PORT || 10000;
 
-/*app.use(express.static(__dirname+'/html'));*/
-app.get('/', (req, res) => res.sendFile('/index.html'));
+app.use(express.static(__dirname+'/html'));
+app.get('/', (req, res) => res.sendFile('index.html'));
 const server = app.listen(port,host, () => console.log('Server Start!!!'));
 
 const io = require('socket.io')(server);
 
-//allPlayers = [];
-//io.on("connection", (socket) => {
-//    socket.on("allPlayer", (s) => {
-//        if (allPlayers.length <= 2) {
-//            allPlayers.push(s.name);
-//        }
-//        if (allPlayers.length >=2 ) {
-//            io.emit("playersComplete", { players:allPlayers });
-//        }
-//    });
-//    socket.on("userScore", (score) => {
-//        socket.broadcast.emit("userScore", { userScore: score.score})
-//    });
-//});
+allPlayers = [];
+io.on("connection", (socket) => {
+    socket.on("allPlayer", (s) => {
+        if (allPlayers.length <= 2) {
+            allPlayers.push(s.name);
+        }
+        if (allPlayers.length >=2 ) {
+            io.emit("playersComplete", { players:allPlayers });
+        }
+    });
+    socket.on("userScore", (score) => {
+       socket.broadcast.emit("userScore", { userScore: score.score})
+    });
+});
 
